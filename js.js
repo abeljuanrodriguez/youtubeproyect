f73b6778073480d5b2dd22522a5f2e8c1108fe0f
Abel Juan Rodriguez <abelelentrego@gmail.com>
2018-10-16 16:38:29 +0200

    cambios estilo v1

--- js.js
+++ js.js
@@ -35,7 +35,7 @@ function showResults(results) {
     var entries = results.items;
     
     $.each(entries, function (index, value) {
-        html += '<div><iframe width="420" height="315" src="https://www.youtube.com/embed/' +value.id.videoId + '"' + '> </iframe><a href="http://www.convertmp3.io/fetch/?video=https://www.youtube.com/watch?v='+ value.id.videoId +'"target="_blank">link text</a></div>';
+        html += '<div><iframe width="80%" height="20%" src="https://www.youtube.com/embed/' +value.id.videoId + '"' + '> </iframe><a href="http://www.convertmp3.io/fetch/?video=https://www.youtube.com/watch?v='+ value.id.videoId +'"target="_blank">link text</a></div>';
     }); 
     
     $('#search-results').html(html);
